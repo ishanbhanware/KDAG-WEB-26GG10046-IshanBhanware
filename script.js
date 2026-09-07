3e70fa08fdfa2ed6bfcfe18c8ed5465007fca7e6
Ishan Bhanware <bhanwareishan@gmail.com>
2026-09-07 11:19:58 +0530

    Commit-5: Deadline extension- final changes and touch ups

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-document.getElementById('registrationForm').addEventListener('submit', function (e) {
-      e.preventDefault();
-
-      const fullname = document.getElementById('fullname').value.trim();
-      const rollNumber = document.getElementById('rollNumber').value.trim();
-      const email = document.getElementById('email').value.trim();
-      const department = document.getElementById('department').value;
-
-      const errorMessage = document.getElementById('errorMessage');
-      const formElement = document.getElementById('registrationForm');
-      const successMessage = document.getElementById('successMessage');
-
-      errorMessage.style.display = 'none';
-      if (!fullname || !rollNumber || !email || !department) {
-        errorMessage.textContent = 'Please fill out all required fields.';
-        errorMessage.style.display = 'block';
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const roll = document.getElementById("rollnumber");
+const department = document.getElementById("department");
+const register = document.getElementById("submit");
+const form = document.getElementById("form");
+const success = document.getElementById("success");
+register.addEventListener("click", () => {
+    if (name.value === "" || email.value === "" || roll.value === "" || department.value === "") {
+        alert("Please fill out all required fields.");
         return;
-      }
-      if (rollNumber.length !== 9) {
-        errorMessage.textContent = 'Roll Number must be exactly 9 characters long (e.g., 26XX10001).';
-        errorMessage.style.display = 'block';
+    }
+    if (roll.value.length !== 9) {
+        alert("Roll Number must be exactly 9 characters long (e.g., 26XX10001).");
         return;
-      }
-      formElement.style.display = 'none';
-      successMessage.style.display = 'block';
-    });
+    }
+    else {
+        form.style.display = "none";
+        success.style.display = "block";
+    }
+});
